f1c597d6e62685dd221f4130d992a4371a4f1660
dytiger <dytiger@163.com>
2017-08-28 22:15:40 +0800

    initial import

--- js/basic.js
+++ js/basic.js
@@ -1,26 +1,30 @@
-$(function(){
-    $('[name="hand"]').add('[name="fingure"]').on('change',function(){
+$(function () {
+    $('[name="hand"]').add('[name="fingure"]').on('change', function () {
         var handValue = $('[name="hand"]:checked').val();
         var fingureValue = $('[name="fingure"]:checked').val();
-        if(handValue&&fingureValue){
+        if (handValue && fingureValue) {
             var classValue = '.' + handValue + fingureValue;
             $('.key').removeClass('current-key');
             $(classValue).addClass('current-key');
         }
     });
 
-    $('html').on('keydown',function(event){
-        var k = '#k'+event.keyCode;
+    $('html').on('keydown', function (event) {
+        var keyCode = event.keyCode;
+        if (keyCode == 9 || keyCode == 32 || keyCode == 13) {
+            event.preventDefault();
+        }
+        var k = '#k' + keyCode;
         $(k).addClass("kd");
     });
-    $('html').on('keyup',function(event){
-        var k = '#k'+event.keyCode;
+    $('html').on('keyup', function (event) {
+        var k = '#k' + event.keyCode;
         $(k).removeClass("kd");
     });
 
-    $('#reset').on('click',function(){
-        $('[name="hand"]:checked').prop('checked',false);
-        $('[name="fingure"]:checked').prop('checked',false);
+    $('#reset').on('click', function () {
+        $('[name="hand"]:checked').prop('checked', false);
+        $('[name="fingure"]:checked').prop('checked', false);
         $('.key').removeClass('current-key');
     });
 });
