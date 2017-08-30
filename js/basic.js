d359a5913907c26495ba4871a812495b55c86715
dytiger <dytiger@163.com>
2017-08-30 14:52:40 +0800

    initial import

--- js/basic.js
+++ js/basic.js
@@ -1,4 +1,4 @@
-var exKeys = [8,9,13,32,37,39,112,114,116,121,122,222,191];
+var exKeys = [8, 9, 13, 18, 32, 37, 39, 112, 114, 116, 121, 122, 222, 191];
 $(function () {
     $('[name="hand"]').add('[name="fingure"]').on('change', function () {
         var handValue = $('[name="hand"]:checked').val();
@@ -10,22 +10,52 @@ $(function () {
         }
     });
 
-    $('html').on('keydown', function (event) {
-        var keyCode = event.keyCode;
-        if(exKeys.indexOf(keyCode)!=-1){
-            event.preventDefault();
-        }
-        var k = '#k' + keyCode;
-        $(k).addClass("kd");
-    });
-    $('html').on('keyup', function (event) {
-        var k = '#k' + event.keyCode;
-        $(k).removeClass("kd");
-    });
-
     $('#reset').on('click', function () {
         $('[name="hand"]:checked').prop('checked', false);
         $('[name="fingure"]:checked').prop('checked', false);
         $('.key').removeClass('highlight-key');
     });
 });
+
+var getLocation = function(){
+
+}
+
+var keysDown = function (event) {
+    var keyCode = event.keyCode;
+    if (exKeys.indexOf(keyCode) != -1) {
+        event.preventDefault();
+    }
+    var k = '#k' + keyCode;
+
+    if (event.shiftKey || event.ctrlKey || event.altKey) {
+        if (event.location === KeyboardEvent.DOM_KEY_LOCATION_LEFT) {
+            k = k + 'l';
+        }
+        if (event.location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
+            k = k + 'r';
+        }
+    }
+    $(k).addClass("kd");
+};
+
+var keysUp = function (event) {
+    var keyCode = event.keyCode;
+    var k = '#k' + keyCode;
+    if (exKeys.indexOf(keyCode) != -1) {
+        event.preventDefault();
+    }
+
+    if (k == '#k16' || k == '#k17' || k == '#k18') {
+        if (event.location === KeyboardEvent.DOM_KEY_LOCATION_LEFT) {
+            k = k + 'l';
+        }
+        if (event.location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
+            k = k + 'r';
+        }
+    }
+    $(k).removeClass("kd");
+}
+
+document.addEventListener('keydown', keysDown);
+document.addEventListener('keyup', keysUp);
